8397d38ba42a30fae75a67f1a2bcf34525d7559a
HyunsuKim <popogustn@naver.com>
2023-04-25 16:44:50 +0900

    project init

---
+++ src/main/java/com/example/coupleSchedule/CoupleScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.coupleSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupleScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupleScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coupleSchedule/CoupleScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coupleSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupleScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
