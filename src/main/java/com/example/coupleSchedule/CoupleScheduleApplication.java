59ad2ea77f6a300efc3a66326cb2e42a7a6ac8aa
hyunsukimT <hyunsu_kim@tmax.co.kr>
2023-06-28 15:27:59 +0900

    test git login

--- src/main/java/com/example/coupleSchedule/CoupleScheduleApplication.java
+++ src/main/java/com/example/coupleSchedule/CoupleScheduleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoupleScheduleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoupleScheduleApplication.class, args);
+		SpringApplication.run(CoupleScheduleApplication.class, args); 
 	}
 
 }
